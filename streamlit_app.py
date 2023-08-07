11d89e93ef73d4d45bb001b87ba57b83fdc88db2
bhushansurve012 <63290183+bhushansurve012@users.noreply.github.com>
2023-08-08 00:37:56 +0530

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title('hello this is badge 2')
+streamlit.header('Breakfast menu')
+streamlit.text('omega 3 & blueberry ')
+streamlit.text('kale')
+streamlit.text('hard-boiled')
+
